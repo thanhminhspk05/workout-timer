09f281f9e1f722ed1e3457ff2e05dddfe95b1967
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-07-03 14:51:19 +0700

    practice with memo, useMemo

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import Calculator from "./Calculator";
+import ToggleSounds from "./ToggleSounds";
+
+// if move workout outside app component, it is constant object, so it not re-create 
+const workouts = [
+  {
+    name: "Full-body workout",
+    numExercises: 8, //  partOfDay === "AM" ? 9 : 8,
+  },
+  {
+    name: "Arms + Legs",
+    numExercises: 6,
+  },
+  {
+    name: "Arms only",
+    numExercises: 3,
+  },
+  {
+    name: "Legs only",
+    numExercises: 4,
+  },
+  {
+    name: "Core only",
+    numExercises: 4 // partOfDay === "AM" ? 5 : 4,
+  },
+];
+
+function App() {
+  const [allowSound, setAllowSound] = useState(true);
+  const [time, setTime] = useState(formatTime(new Date()));
+
+  // Will be be AM or PM
+  // const partOfDay = time.slice(-2);
+
+
+  function formatTime(date) {
+    return new Intl.DateTimeFormat("en", {
+      month: "short",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(date);
+  }
+
+  useEffect(function () {
+    const id = setInterval(function () {
+      setTime(formatTime(new Date()));
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <main>
+      <h1>Workout timer</h1>
+      <time>For your workout on {time}</time>
+      {/* allowSound is primitive value, not change between render, so it will not re-render when using memo */}
+      <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} /> 
+
+      {/* allowSound is primitive value but workouts is an objectm, it will re-created every single render */}
+      <Calculator workouts={workouts} allowSound={allowSound} />
+    </main>
+  );
+}
+
+export default App;
