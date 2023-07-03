380c0380341e4ce6f857e2abcd65624abbd5e3e6
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-07-03 14:57:44 +0700

    optimize, useMeno, move const func outside app component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSounds from "./ToggleSounds";
 
-// if move workout outside app component, it is constant object, so it not re-create 
-const workouts = [
-  {
-    name: "Full-body workout",
-    numExercises: 8, //  partOfDay === "AM" ? 9 : 8,
-  },
-  {
-    name: "Arms + Legs",
-    numExercises: 6,
-  },
-  {
-    name: "Arms only",
-    numExercises: 3,
-  },
-  {
-    name: "Legs only",
-    numExercises: 4,
-  },
-  {
-    name: "Core only",
-    numExercises: 4 // partOfDay === "AM" ? 5 : 4,
-  },
-];
+// constants - no need to re-create functions
+function formatTime(date) {
+  return new Intl.DateTimeFormat("en", {
+    month: "short",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(date);
+}
+
 
 function App() {
   const [allowSound, setAllowSound] = useState(true);
   const [time, setTime] = useState(formatTime(new Date()));
 
   // Will be be AM or PM
-  // const partOfDay = time.slice(-2);
+  const partOfDay = time.slice(-2);
 
+  const workouts = useMemo(() => {
+    return [
+      {
+        name: "Full-body workout",
+        numExercises: partOfDay === "AM" ? 9 : 8,
+      },
+      {
+        name: "Arms + Legs",
+        numExercises: 6,
+      },
+      {
+        name: "Arms only",
+        numExercises: 3,
+      },
+      {
+        name: "Legs only",
+        numExercises: 4,
+      },
+      {
+        name: "Core only",
+        numExercises: partOfDay === "AM" ? 5 : 4,
+      },
+    ];
+  }, [partOfDay])
 
-  function formatTime(date) {
-    return new Intl.DateTimeFormat("en", {
-      month: "short",
-      year: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    }).format(date);
-  }
 
   useEffect(function () {
     const id = setInterval(function () {
